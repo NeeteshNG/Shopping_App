a7cf1c3e1e329dda2006ed9958c1df674178ab18
Neetesh NG <neeteshng15@gmail.com>
2023-10-26 12:54:36 +0530

    Improved WishList Button

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addToCart, addToWishlist } from "../Redux/listSlice";
 import ProductDetails from "./ProductDetails";
 import { Link } from "react-router-dom";
+import { removeFromWishlist } from "../Redux/listSlice";
 
 function ProductCard({ product }) {
   const { id, name, price, description, images } = product;
   const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
+
+  const wishlist = useSelector((state) => state.list.wishlist)
+  const [isInWishlist, setIsInWishlist] = useState(wishlist.some(item => item.id === product.id));
+
+  const toggleWishlist = () => {
+    if (isInWishlist) {
+      dispatch(removeFromWishlist(product));
+    } else {
+      dispatch(addToWishlist(product));
+    }
+    setIsInWishlist(!isInWishlist);
+  };
+
 
   const handleAddToCart = () => {
     dispatch(addToCart(product));
@@ -26,14 +40,14 @@ function ProductCard({ product }) {
       <Link to={`/products/${id}`}>
         <img src={images} alt={name} />
       </Link>
+      <div className={`wishlist-button ${isInWishlist ? 'filled' : ''}`} onClick={toggleWishlist}>
+        <i className="fa fa-heart"></i>
+      </div>
       <h2>{name}</h2>
       <p>{description.slice(0, 40)}...</p>
       <p>Price : {price} /-</p>
       <button onClick={handleAddToCart}>
         <i className="fa fa-cart-plus"></i>
-      </button>
-      <button onClick={handleAddToWishlist}>
-        <i className="fa fa-heart-o"></i>
       </button>
       <button onClick={handleToggle}>
         <i className="fa fa-info-circle"></i>
