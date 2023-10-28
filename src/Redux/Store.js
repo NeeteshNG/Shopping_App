1c7eae144ffec392308e62fd67ea2970eee817c7
NG <72024263+NeeteshNG@users.noreply.github.com>
2023-10-28 08:07:04 +0000

    Removed Redux Persist.

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,30 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import listReducer from "./listSlice";
-import userReducer from "../UserAuth/Redux_User/userSlice"
-import storage from "redux-persist/lib/storage";
-import { persistStore, persistReducer } from "redux-persist";
-
-const persistConfig = {
-    key : 'root',
-    storage
-}
-
-const combinePersistForList = persistReducer(persistConfig, listReducer)
-
-const combinePersistForUser = persistReducer(persistConfig, userReducer)
+import userReducer from "../UserAuth/Redux_User/userSlice";
 
 const store = configureStore({
-    reducer:{
-        list : combinePersistForList,
-        user : combinePersistForUser
-    },
-    middleware: getDefaultMiddleware => getDefaultMiddleware({
-        serializableCheck:{
-            ignoreActions : ['persist/PERSIST'],
-        },
-    })
-})
+  reducer: {
+    list: listReducer,
+    user: userReducer,
+  },
+});
 
-const persistor = persistStore(store);
-
-export {persistor, store}
+export default store;
--- src/index.js
+++ src/index.js
@@ -4,18 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { persistor, store } from "./Redux/Store";
-import { PersistGate } from "redux-persist/integration/react";
+import store from "./Redux/Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor} loading={null}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 reportWebVitals();
