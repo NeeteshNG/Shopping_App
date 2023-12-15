af1a11c97d272964ce3857b99a7798c5946b8935
Neetesh NG <neeteshng15@gmail.com>
2023-12-15 16:09:27 +0530

    Separated the Homepage in Different Folder.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Home from "./Components/Home";
+import Home from "./Components/Homepage/Home";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Products from "./Components/Products";
---
+++ src/Components/Homepage/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SlidingPage from "../Slidingpage/Slidingpage";
+import "../Homepage/Home.css"
+
+function Home() {
+  return (
+    <div className="home-page">
+      <SlidingPage/>
+    </div>
+  );
+}
+
+export default Home;
