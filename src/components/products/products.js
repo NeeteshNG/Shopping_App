ada214d7104c9997f3f09965c6c6f19f2dd559ed
Neetesh NG <neeteshng15@gmail.com>
2023-12-15 18:21:32 +0530

    Made Filter Bar in Side Left.

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -1,27 +1,47 @@
 import React, { useState } from "react";
-import "./products.css"
+import "./products.css";
 import ProductCard from "../productcard/productCard";
 
 function Products({ products }) {
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedSorting, setSelectedSorting] = useState("");
+  const [showFilters, setShowFilters] = useState(false);
 
   const handleCategoryChange = (event) => {
     setSelectedCategory(event.target.value);
   };
 
-  const filteredProducts = selectedCategory
-    ? products.filter((product) => product.category === selectedCategory)
-    : products;
+  const handleSortingChange = (event) => {
+    setSelectedSorting(event.target.value);
+  };
+
+  let filteredProducts = products;
+
+  if (selectedCategory) {
+    filteredProducts = filteredProducts.filter(
+      (product) => product.category === selectedCategory
+    );
+  }
+
+  if (selectedSorting === "price_low_to_high") {
+    filteredProducts.sort((a, b) => a.price - b.price);
+  } else if (selectedSorting === "price_high_to_low") {
+    filteredProducts.sort((a, b) => b.price - a.price);
+  }
 
   const categoryOptions = ["men", "women", "kids"];
+  const sortingOptions = [
+    { label: "L -- H", value: "price_low_to_high" },
+    { label: "H -- L", value: "price_high_to_low" },
+  ];
 
   return (
     <div className="body-products">
-      <div className="category-filter">
-        <div className="box">
-        <label className="filter-text" htmlFor="category">
-          FILTER BY CATEGORY :{" "}
-        </label>
+      <div className="filter-section">
+        <div className="filter-box">
+          <label className="filter-text" htmlFor="category">
+            FILTER BY CATEGORY :{" "}
+          </label>
           <select
             id="category"
             value={selectedCategory}
@@ -31,6 +51,23 @@ function Products({ products }) {
             {categoryOptions.map((category, index) => (
               <option key={index} value={category}>
                 {category}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="filter-box">
+          <label className="filter-text" htmlFor="sorting">
+            SORT BY PRICE:{" "}
+          </label>
+          <select
+            id="sorting"
+            value={selectedSorting}
+            onChange={handleSortingChange}
+          >
+            {sortingOptions.map((option, index) => (
+              <option key={index} value={option.value}>
+                {option.label}
               </option>
             ))}
           </select>
